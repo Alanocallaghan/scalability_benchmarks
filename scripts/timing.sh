3fe874098617e200bf3edced634d9be9ea9fe805
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-10-15 19:05:07 +0100

    Correct modules

--- scripts/timing.sh
+++ scripts/timing.sh
@@ -7,6 +7,6 @@
 . /etc/profile.d/modules.sh
 MODULEPATH=$MODULEPATH:/exports/igmm/software/etc/el7/modules
 module load igmm/compilers/gcc/5.5.0
-module load igmm/apps/R/3.5.0
+module load igmm/apps/R/3.6.0
 
 Rscript scripts/timing.R
