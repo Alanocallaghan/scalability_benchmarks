808fe179d3e8c6d29170b7b23b94fb1213b1c220
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-10-30 16:14:31 +0000

    3.6.1 not 3.6.0

--- scripts/chain-scripts/advi.sh
+++ scripts/chain-scripts/advi.sh
@@ -7,7 +7,7 @@
 . /etc/profile.d/modules.sh
 MODULEPATH=$MODULEPATH:/exports/igmm/software/etc/el7/modules
 module load igmm/compilers/gcc/5.5.0
-module load igmm/apps/R/3.6.0
+module load igmm/apps/R/3.6.1
 
 settings=($(sed -n "$SGE_TASK_ID p" data/advi_grid.txt))
 
