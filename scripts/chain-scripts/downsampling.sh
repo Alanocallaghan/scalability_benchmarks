0c5fe60be4f364db54884f5c2fcf86b99c71c9d5
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-10-30 16:13:52 +0000

    Downsampling code

--- scripts/chain-scripts/downsampling.sh
+++ scripts/chain-scripts/downsampling.sh
@@ -10,6 +10,6 @@ MODULEPATH=$MODULEPATH:/exports/igmm/software/etc/el7/modules
 module load igmm/compilers/gcc/5.5.0
 module load igmm/apps/R/3.6.0
 
-settings=($(sed -n "$SGE_TASK_ID p" data/divide_and_conquer_grid.txt))
+settings=($(sed -n "$SGE_TASK_ID p" data/downsampling.txt))
 
-Rscript scripts/chain-scripts/divide_and_conquer.R ${settings[@]} /exports/eddie/scratch/s1372510/divide_and_conquer/$SGE_TASK_ID
+Rscript scripts/chain-scripts/downsampling.R ${settings[@]} /exports/eddie/scratch/s1372510/downsampling/$SGE_TASK_ID
