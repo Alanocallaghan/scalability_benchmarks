7c51d6cdc37f73a3b7a42ef749509f59d1a98b11
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-11-07 12:09:45 +0000

    Use only batch for ID

--- scripts/chain-scripts/identifiable.sh
+++ scripts/chain-scripts/identifiable.sh
@@ -9,6 +9,6 @@ MODULEPATH=$MODULEPATH:/exports/igmm/software/etc/el7/modules
 module load igmm/compilers/gcc/5.5.0
 module load igmm/apps/R/3.6.1
 
-settings=($(sed -n "$SGE_TASK_ID p" data/datasets.txt))
+settings=($(sed -n "$SGE_TASK_ID p" data/datasets_batch.txt))
 
 Rscript scripts/chain-scripts/identifiable.R ${settings[@]} /exports/eddie/scratch/s1372510/identifiable/$SGE_TASK_ID
