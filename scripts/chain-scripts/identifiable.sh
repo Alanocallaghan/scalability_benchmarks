310edbc20e3c19044679f6f1b48b5f9009737457
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-11-12 15:55:45 +0000

    Minor edits to make things less wrong

--- scripts/chain-scripts/identifiable.sh
+++ scripts/chain-scripts/identifiable.sh
@@ -12,4 +12,4 @@ module load igmm/apps/R/3.6.1
 settings=($(sed -n "$SGE_TASK_ID p" data/datasets_batch.txt))
 
 Rscript scripts/chain-scripts/identifiable.R ${settings[@]} outputs/identifiable/$SGE_TASK_ID
-Rscript scripts/chain-scripts/non_identifiable.R ${settings[@]} outputs/identifiable/$SGE_TASK_ID
+# Rscript scripts/chain-scripts/non_identifiable.R ${settings[@]} outputs/identifiable/$SGE_TASK_ID
