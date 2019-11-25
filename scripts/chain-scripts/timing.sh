1916d80f07099e4bdc0e174bf1d315d1af042553
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-11-25 12:19:45 +0000

    Random changes

--- scripts/chain-scripts/timing.sh
+++ scripts/chain-scripts/timing.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -f
 #$ -S /bin/bash
-#$ -l h_vmem=5G
+#$ -l h_vmem=10G
 #$ -cwd
-#$ -l h_rt=72:00:00
+#$ -l h_rt=100:00:00
 
 . /etc/profile.d/modules.sh
 MODULEPATH=$MODULEPATH:/exports/igmm/software/etc/el7/modules
 module load igmm/compilers/gcc/5.5.0
 module load igmm/apps/R/3.6.1
 
-Rscript scripts/chain-scripts/timing.R tung
-Rscript scripts/chain-scripts/timing.R zeisel
+# Rscript scripts/chain-scripts/timing.R tung
+# Rscript scripts/chain-scripts/timing.R zeisel
 Rscript scripts/chain-scripts/timing.R pbmc
-Rscript scripts/chain-scripts/timing.R buettner
+# Rscript scripts/chain-scripts/timing.R buettner
