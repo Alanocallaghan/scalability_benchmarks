48965a2d9316f0b9307cb441e2da8976feb4c33e
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-11-25 12:06:01 +0000

    Bugfix

--- scripts/chain-scripts/removing_cells.R
+++ scripts/chain-scripts/removing_cells.R
@@ -19,7 +19,7 @@ frac <- as.numeric(args[[2]])
 data <- data[, sample(ncol(counts(data)), floor(ncol(counts(data)) * frac))]
 if (length(altExpNames(data))) {
   spikes <- altExp(data, "spike-ins")
-  spikes <- spikes[rowSums(assay(spikes) != 0, ]
+  spikes <- spikes[rowSums(assay(spikes)) != 0, ]
   altExp(data, "spike-ins") <- spikes
 }
 
