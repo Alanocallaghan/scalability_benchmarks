b4cd24432efcc9a78fcf2237487fcccf367eeb15
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2020-02-21 17:07:32 +0000

    Local for debugging

--- scripts/chain-scripts/divide_and_conquer.R
+++ scripts/chain-scripts/divide_and_conquer.R
@@ -1,11 +1,11 @@
 #!/usr/bin/env Rscript
 
 library("here")
-library("BASiCS")
+devtools::load_all("../BASiCS")
 # library("Scalability")
 devtools::load_all("../Scalability")
 library("future")
-plan("multicore")
+# plan("multicore")
 
 args <- commandArgs(trailingOnly = TRUE)
 
@@ -15,6 +15,8 @@ source(here("scripts/chain-scripts/benchmark_code.R"))
 data <- readRDS(here("data", paste0(args[[1]], ".rds")))
 dir <- args[[5]]
 dir.create(dir, recursive = TRUE, showWarnings = FALSE)
+
+
 
 
 data <- divide_and_conquer_benchmark(
