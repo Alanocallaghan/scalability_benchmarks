c6b69d4251e9ed8de0d4d256f14a9586402fdb3b
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2020-02-22 11:29:25 +0000

    Fix I hope

--- scripts/chain-scripts/divide_and_conquer.R
+++ scripts/chain-scripts/divide_and_conquer.R
@@ -1,11 +1,12 @@
 #!/usr/bin/env Rscript
 
 library("here")
-library("BASiCS")
+devtools::load_all("../BASiCS")
 # library("Scalability")
-devtools::load_all("../Scalability")
+# devtools::load_all("../Scalability")
 library("future")
 plan("multicore")
+options(stringsAsFactors=FALSE)
 
 args <- commandArgs(trailingOnly = TRUE)
 
@@ -24,7 +25,7 @@ data <- divide_and_conquer_benchmark(
   NSubsets = args[[2]],
   Seed = args[[3]],
   Regression = TRUE,
-  MinGenesPerRBF = 1,
+  MinGenesPerRBF = 100,
   N = 20000,
   Thin = 10,
   Burn = 10000
@@ -45,3 +46,23 @@ if (length(chains) == 1) {
 saveRDS(data[["chain"]], file = file.path(dir, "chains.rds"))
 saveRDS(data[["time"]], file = file.path(dir, "time.rds"))
 saveRDS(data[["config"]], file = file.path(dir, "config.rds"))
+
+
+# grid <- read.delim("data/divide_and_conquer_grid.txt", header=FALSE)
+# source(here("scripts/chain-scripts/benchmark_code.R"))
+# out <- lapply(1:nrow(grid), function(i) {
+#   args <- grid[i, ]
+#   data <- readRDS(here("data", paste0(args[[1]], ".rds")))
+#   divide_and_conquer_benchmark(
+#     Data = data,
+#     DataName = args[[1]],
+#     SubsetBy = args[[4]],
+#     NSubsets = args[[2]],
+#     Seed = args[[3]],
+#     Regression = TRUE,
+#     MinGenesPerRBF = 100,
+#     N = 20,
+#     Thin = 4,
+#     Burn = 8
+#   )
+# })
