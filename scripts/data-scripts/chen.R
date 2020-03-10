f626762b94bf67556e8bdea65353f6b772eac69a
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2020-03-10 12:37:42 +0000

    Remove plots, reorder dc/advi

--- scripts/data-scripts/chen.R
+++ scripts/data-scripts/chen.R
@@ -6,9 +6,9 @@ sce <- readRDS("downloads/chen.rds")
 
 sce <- sce[, sce$cell_type1 == "Astro"]
 
-hist(log10(colSums(counts(sce))), breaks = "FD")
-hist(log10(rowMeans(counts(sce))), breaks = "FD")
-hist(rowMeans(counts(sce) != 0), breaks = "FD")
+# hist(log10(colSums(counts(sce))), breaks = "FD")
+# hist(log10(rowMeans(counts(sce))), breaks = "FD")
+# hist(rowMeans(counts(sce) != 0), breaks = "FD")
 
 
 sce <- sce[rowMeans(counts(sce)) > 0.1, ]
