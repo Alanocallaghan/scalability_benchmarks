776499e83fc9e4dbd813b2d6650ef18a56c923a1
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2021-06-09 10:40:42 +0100

    advi

---
+++ src/chains/true_positives_advi.R
@@ -0,0 +1,45 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages({
+  library("argparse")
+  library("here")
+  library("BASiCStan")
+})
+parser <- ArgumentParser()
+parser$add_argument("-s", "--seed", type = "double")
+parser$add_argument("-o", "--output")
+args <- parser$parse_args()
+
+set.seed(args[["seed"]])
+
+droplet_sce <- readRDS("data/ibarra-soria.rds")
+
+# Presomitic mesoderm
+ind_presom <- colData(droplet_sce)[["Cell_type"]] == "PSM"
+ind_som <- colData(droplet_sce)[["Cell_type"]] == "SM"
+
+PSM_Data <- droplet_sce[, ind_presom]
+SM_Data <- droplet_sce[, ind_som]
+
+# Presomitic mesoderm cells
+PSM_MCMC <- BASiCStan(
+  PSM_Data,
+  Regression = TRUE,
+  WithSpikes = FALSE
+)
+
+# Somitic mesoderm cells
+SM_MCMC <- BASiCStan(
+  SM_Data,
+  Regression = TRUE,
+  WithSpikes = FALSE
+)
+
+test <- BASiCS_TestDE(PSM_MCMC, SM_MCMC)
+
+out <- list(
+  test = test,
+  mcmc = list(sm = SM_MCMC, psm = PSM_MCMC)
+)
+
+saveRDS(out, args[["output"]])
