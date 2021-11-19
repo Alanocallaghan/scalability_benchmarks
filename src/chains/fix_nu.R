2f931a6610257086e9395cff7f3919f0ad912aba
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2021-11-19 00:55:55 +0000

    Actual fix nu code

--- src/chains/fix_nu.R
+++ src/chains/fix_nu.R
@@ -34,6 +34,61 @@ fit_var <- BASiCS_MCMC(
   Burn = max(args[["iterations"]] / 2, 4)
 )
 
-dir.create(args[["output"]])
+
+dir.create(args[["output"]], showWarnings = FALSE)
 saveRDS(fit_fix, file.path(args[["output"]], "fix.rds"))
 saveRDS(fit_var, file.path(args[["output"]], "var.rds"))
+
+sce <- readRDS("rdata/ibarra-soria.rds")
+
+
+PSM_Data <- droplet_sce[, ind_presom]
+SM_Data <- droplet_sce[, ind_som]
+
+fit_fix <- BASiCS_MCMC(
+  PSM_Data,
+  PrintProgress = FALSE,
+  FixNu = TRUE,
+  WithSpikes = FALSE,
+  Regression = TRUE,
+  N = args[["iterations"]],
+  Thin = max((args[["iterations"]] / 2) / 1000, 2),
+  Burn = max(args[["iterations"]] / 2, 4)
+)
+
+fit_var <- BASiCS_MCMC(
+  PSM_Data,
+  PrintProgress = FALSE,
+  FixNu = FALSE,
+  WithSpikes = FALSE,
+  Regression = TRUE,
+  N = args[["iterations"]],
+  Thin = max((args[["iterations"]] / 2) / 1000, 2),
+  Burn = max(args[["iterations"]] / 2, 4)
+)
+saveRDS(fit_fix, file.path(args[["output"]], "ibarra-presom-fix.rds"))
+saveRDS(fit_var, file.path(args[["output"]], "ibarra-presom-var.rds"))
+
+fit_fix <- BASiCS_MCMC(
+  SM_Data,
+  PrintProgress = FALSE,
+  FixNu = TRUE,
+  WithSpikes = FALSE,
+  Regression = TRUE,
+  N = args[["iterations"]],
+  Thin = max((args[["iterations"]] / 2) / 1000, 2),
+  Burn = max(args[["iterations"]] / 2, 4)
+)
+
+fit_var <- BASiCS_MCMC(
+  SM_Data,
+  PrintProgress = FALSE,
+  FixNu = FALSE,
+  WithSpikes = FALSE,
+  Regression = TRUE,
+  N = args[["iterations"]],
+  Thin = max((args[["iterations"]] / 2) / 1000, 2),
+  Burn = max(args[["iterations"]] / 2, 4)
+)
+saveRDS(fit_fix, file.path(args[["output"]], "ibarra-som-fix.rds"))
+saveRDS(fit_var, file.path(args[["output"]], "ibarra-som-var.rds"))
