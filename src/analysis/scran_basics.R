899c722532399d9e564088eb3219bea22ceae74e
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2021-11-27 01:30:12 +0000

    More scran

--- src/analysis/scran_basics.R
+++ src/analysis/scran_basics.R
@@ -1,5 +1,6 @@
 ## comparing scran and BASiCS
 library("ggplot2")
+library("cowplot")
 library("viridis")
 library("scran")
 library("ggpointdensity")
@@ -26,30 +27,35 @@ r <- range(c(norm_comp_df$scran, norm_comp_df$BASiCS))
 # r <- range(c(sf, bf))
 # chen_sf_scran <- data.frame(scran = sf, BASiCS = bf)
 
-g <- ggplot(norm_comp_df) +
-    aes(scran, BASiCS) +
-    geom_pointdensity() +
-    facet_wrap(~data) +
-    scale_x_log10(limits = r) +
-    scale_y_log10(limits = r) +
-    scale_colour_viridis(name = "Number of neighbours") +
-    labs(x = "scran size factors", y = "BASiCS size factors") +
-    geom_text(
-        x = 0.3, y = 10,
-        hjust = 0,
-        mapping = aes(
-            label = sprintf(
-                "Peason's rho: %0.03f", cor(scran, BASiCS)
-            )
-        )
-    ) +
-    theme_bw() +
-    theme(
-        # panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank()
-    ) +
-    theme(legend.position = "bottom")
 
+gs <- lapply(
+    unique(norm_comp_df$data),
+    function(d) {
+        norm_comp_sub <- norm_comp_df[norm_comp_df$data == d, ]
+        ggplot(norm_comp_sub) +
+            aes(scran, BASiCS) +
+            geom_pointdensity() +
+            scale_x_log10(limits = r) +
+            scale_y_log10(limits = r) +
+            scale_colour_viridis(name = "Number of neighbours") +
+            labs(x = "scran size factors", y = "BASiCS size factors") +
+            annotate(
+                geom = "text",
+                x = 0.3, y = 10.1,
+                hjust = 0,
+                vjust = 0,
+                label = sprintf(
+                    "Peason's rho: %0.03f",
+                    cor(norm_comp_sub$scran, norm_comp_sub$BASiCS)
+                )
+            ) +
+            theme_bw() +
+            theme(
+                # panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank()
+            ) +
+            theme(legend.position = "none")
+    }
+)
+plot_grid(plotlist = gs, nrow = 1, labels = "AUTO")
 ggsave("figs/scran_basics.pdf", width = 8, height = 4)
-
-
